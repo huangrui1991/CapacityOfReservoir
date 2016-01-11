6556b01f5db0c5a9fbe52aac9474856a6938a5a9
HuangRui <Huangrui1991@outlook.com>
2016-01-11 16:40:53 +0800

    Create DEMGenerater

--- PyFile/Install/DEMCreater.py
+++ PyFile/Install/DEMCreater.py
@@ -1,1 +1,30 @@
 __author__ = 'HR'
+
+import arcpy
+from PyQt4 import QtCore
+from PyQt4 import QtGui
+import sys
+import CreateDEMDialog
+
+class DEMGenerater():
+    def __init__(self):
+        pass
+
+    InputFilePath = QtCore.QString()
+
+    @QtCore.pyqtSlot()
+    def SelectFilePath(self):
+        InputFilePath = QtGui.QFileDialog.getOpenFileName()
+        if(InputFilePath != None):
+            pass
+
+class DEMCreater(QtGui.QDialog,CreateDEMDialog.Ui_CreateDEMDialog):
+    __demGenerater__ = DEMGenerater()
+
+    def __init__(self,parent = 0):
+        super(DEMCreater, self).__init__(parent)
+        self.setupUi(self,CreateDEMDialog)
+        QtCore.QObject.connect(self,self.InputFeatureClassPathButton,"click(self)",self.__demGenerater__,"SelectFilePath(self)")
+
+
+
